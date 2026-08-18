744950ecce86ea738edf7775a5a85cdb2222b7a7
JRDNGUERRERO <j.guerreromoraga@uandresbello.edu>
2026-08-17 22:22:24 -0400

    Add files via upload

---
+++ Semana 2 Jg/empresa.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PAGINA PRINCIPAL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <!-- A grey horizontal navbar that becomes vertical on small screens -->
+    <nav class="navbar navbar-expand-sm bg-secondary">
+
+        <div class="container-fluid">
+            <!-- Links -->
+            <ul class="navbar-nav">
+                <a class="navbar-brand" href="index.php"><i class="fa fa-android"></i></a>
+                <li class="nav-item">
+                    <a class="nav-link" href="contacto.php" style="color:white;">Contacto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="empresa.php" style="color:white;">Empresa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="productos.php" style="color:white;">Productos</a>
+                </li>
+            </ul>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+                <?xml version="1.0" ?><svg style="width: 20px; height: 20px;" viewBox="0 0 32 32"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <title />
+                    <g id="about">
+                        <path d="M16,16A7,7,0,1,0,9,9,7,7,0,0,0,16,16ZM16,4a5,5,0,1,1-5,5A5,5,0,0,1,16,4Z" />
+                        <path
+                            d="M17,18H15A11,11,0,0,0,4,29a1,1,0,0,0,1,1H27a1,1,0,0,0,1-1A11,11,0,0,0,17,18ZM6.06,28A9,9,0,0,1,15,20h2a9,9,0,0,1,8.94,8Z" />
+                    </g>
+                </svg>
+            </button>
+        </div>
+
+    </nav>
+
+
+    <div class="container-fluid" style="background-color: pink;">
+        <div class="row mt-3">
+            <div class="col-6">
+                <center>
+                    <h4 class="card-title">¿Quienes Somos?</h4>
+                    <br>
+
+                    <div class="card h-100" style="width:400px;">
+                        <center>
+                            <img class="card-img-top"
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7pH0U5hIH00nah4qwm-VG8TMeVRKdRLanEg54CHEoyQ&s=10"
+                                alt="Card image" style="width:200px; height:200px">
+                        </center>
+                        <div class="card-body">
+                            <p class="card-text">Somos una empresa que se dedica a crear memes de jugadores de futbol
+                            </p>
+                        </div>
+                    </div>
+                </center>
+
+            </div>
+            <div class="col-6">
+                <center>
+                    <h4 class="card-title">¿Donde Puedes Encontrarnos?</h4>
+                    <br>
+                    <div class="card h-100" style="width: 400px;">
+                        <center>
+                            <?xml version="1.0" ?>
+                            <!DOCTYPE svg
+                                PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>
+                            <svg enable-background="new 0 0 512 512" id="Layer_1" version="1.1" viewBox="0 0 512 512"
+                                xml:space="preserve" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink" style="width:150px; height:150px;">
+                                <g>
+                                    <path
+                                        d="M505,257c0,34.8-0.7,69.7,0.2,104.5c1.5,61.6-37.2,109.2-86.5,130.4c-19.8,8.5-40.6,13-62.1,13c-67.3,0.1-134.7,1-202-0.3   c-50.7-1-92.4-22.2-122.3-64c-15.7-22-23.2-47-23.2-74.1c0-71.7,0-143.3,0-215c0-58.5,28.5-99.4,79.1-126C110.2,14,134.1,9.1,159,9   c65.3,0,130.7-0.4,196,0.2c50.7,0.4,93,19.8,124.2,60.6c17.4,22.8,25.8,49,25.8,77.8C505,184,505,220.5,505,257z M46,257   c0,36.7,0,73.3,0,110c0,16.4,3.8,31.8,12.3,45.7c22.3,36.5,56,54.3,97.8,55c67.1,1,134.3,0.4,201.5,0.2c16.5,0,32.5-3.4,47.4-10.5   c40.6-19.4,63.3-50.3,63.1-96.7c-0.4-71-0.1-142-0.1-213c0-20.1-5.7-38.5-17.6-54.7c-23-31.1-54.8-46.4-92.8-46.8   c-67-0.8-134-0.3-201-0.2c-14.3,0-28.1,2.9-41.5,7.9c-36.8,13.7-71,48.4-69.4,99.5C46.9,188,46,222.5,46,257z" />
+                                    <path
+                                        d="M257.6,363c-64.5,0-116.5-51.4-116.6-115.4c-0.1-63,52.3-114.6,116.4-114.6c64.3-0.1,116.5,51.4,116.6,114.9   C374,311.3,321.9,362.9,257.6,363z M257.6,326c43.9,0,79.5-35.1,79.4-78.3c-0.1-42.8-35.7-77.8-79.4-77.8   c-43.9,0-79.7,34.9-79.7,78C178,291.1,213.7,326.1,257.6,326z" />
+                                    <path
+                                        d="M387.5,98c13.5,0,24.5,11.5,24.5,25.6c-0.1,14.1-11.2,25.5-24.7,25.4c-13.3-0.1-24.2-11.5-24.2-25.3   C363,109.6,374,98,387.5,98z" />
+                                </g>
+                            </svg>
+                        </center>
+
+                        <div class="card-body">
+                            <p class="card-text">Te dejamos aqui nuestra pagina de instagram para que puedas enviarnos
+                                tus
+                                aportes</p>
+                            <a href="">@memesfuchibol</a>
+                        </div>
+                    </div>
+                </center>
+
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="modal fade" id="myModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Ingreso de Usuarios</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+
+                <!-- Modal body -->
+                <form action="acceso.php">
+                    <div class="modal-body">
+
+                        <div class="mb-3 mt-3">
+                            <label for="email" class="form-label">Email:</label>
+                            <input type="email" class="form-control" id="email" placeholder="Ingresar Email"
+                                name="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="pwd" class="form-label">Contraseña:</label>
+                            <input type="password" class="form-control" id="pwd" placeholder="Ingresar Contraseña"
+                                name="pswd">
+                        </div>
+                        <div class="form-check mb-3">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox" name="remember"> Recordar Contraseña
+                            </label>
+                        </div>
+
+                    </div>
+
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Acceder</button>
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
